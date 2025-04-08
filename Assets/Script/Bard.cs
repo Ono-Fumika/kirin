299b8cfe521b5cc19cf193860bbe48cd53523c57
小野 冬美香 <k023g0049@g.neec.ac.jp>
2025-04-09 05:20:08 +0900

    敵修正

--- Assets/Script/Bard.cs
+++ Assets/Script/Bard.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Bard : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float moveSpeed = 2.0f; // Bardの移動速度
+    private Camera mainCamera; // メインカメラの参照
+
     void Start()
     {
-        
+        // メインカメラを取得
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Bardが画面内にいるか判定
+        Vector3 viewportPos = mainCamera.WorldToViewportPoint(transform.position);
+
+        // ビュー内（x: 0～1, y: 0～1, z > 0）で動く、それ以外で停止
+        if (viewportPos.x >= 0 && viewportPos.x <= 1 &&
+            viewportPos.y >= 0 && viewportPos.y <= 1 &&
+            viewportPos.z > 0)
+        {
+            MoveRight(); // 画面内にいる場合は右に動く
+        }
+        // Bardを右に移動させる
+        void MoveRight()
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
     }
 }
